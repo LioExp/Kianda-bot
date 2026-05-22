a073a1c0a83202e9681962ab91f426b1bfe7b87a
Lio Exp <liocosta07@gmail.com>
2026-05-23 00:39:29 +0100

    feat: initial

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./kiandabot.db")
 GREEN_API_INSTANCE_ID = os.getenv("GREEN_API_INSTANCE_ID", "")
 GREEN_API_TOKEN = os.getenv("GREEN_API_TOKEN", "")
 APP_BASE_URL = os.getenv("APP_BASE_URL", "http://localhost:8000")
-GREEN_API_URL = os.getenv("GREEN_API_URL", "https://api.green-api.com")
+GREEN_API_URL = os.getenv("GREEN_API_URL", "https://api.greenapi.com")
 
 COMMISSION_GROUP_RATE = 0.05
 COMMISSION_PLATFORM_RATE = 0.05
